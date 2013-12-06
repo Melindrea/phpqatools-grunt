63f248336f94df5e02e04da6a5848435cbe50a4f
Marie Hogebrandt <marie@pineberry.com>
2013-12-06 13:32:26 +0100

    Adding Gruntfile and Composer

---
+++ Gruntfile.js
@@ -0,0 +1,56 @@
+'use strict';
+
+// # Globbing
+// for performance reasons we're only matching one level down:
+// 'test/spec/{,*/}*.js'
+// use this if you want to recursively match all subfolders:
+// 'test/spec/**/*.js'
+
+module.exports = function (grunt) {
+    // show elapsed time at the end
+    require('time-grunt')(grunt);
+    // load all grunt tasks
+    require('load-grunt-tasks')(grunt);
+    grunt.loadTasks('tasks');
+
+    var directoriesConfig = {
+        composer: 'vendor',
+        composerBin: 'vendor/bin',
+        reports: 'logs',
+        php: 'app'
+    };
+
+    grunt.initConfig({
+        directories: directoriesConfig,
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc',
+                reporter: require('jshint-stylish')
+            },
+            all: [
+                'Gruntfile.js'
+            ]
+        },
+        jsvalidate: {
+            files: [
+                'Gruntfile.js'
+            ]
+        },
+        jsonlint: {
+            files: [
+                '*.json'
+            ]
+        }
+
+    });
+
+    grunt.registerTask('default', [
+        'test'
+    ]);
+
+    grunt.registerTask('test', [
+        'jsvalidate',
+        'jshint',
+        'jsonlint'
+    ]);
+};
