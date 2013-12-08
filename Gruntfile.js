7aa84676ef8c8d17d11cfa528a2152f7857b331b
Marie Hogebrandt <iam@mariehogebrandt.se>
2013-12-08 16:47:03 +0100

    Minor fixes

--- Gruntfile.js
+++ Gruntfile.js
@@ -65,7 +65,7 @@ module.exports = function (grunt) {
                 options: {
                     create: ['<%= directories.reports %>/phpmd']
                 },
-            },
+            }
         },
 
         // JS tasks
